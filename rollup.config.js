8e0cb36200b300131dc34460973c2cb022f391fe
Xiaolong Tang <xxlongtang@gmail.com>
2018-02-20 11:00:11 -0500

    correct rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,30 @@
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-    entry: 'bundles/index.js',
-    dest: 'bundles/ng2-completer.umd.js',
-    format: 'umd',
+    input: 'bundles/index.js',
+    output: {
+        name: 'ng2Completer',        
+        format: 'umd',
+        file: 'bundles/ng2-completer.umd.js',
+        sourcemap: true,        
+        globals: {
+            '@angular/core': 'ng.core',
+            '@angular/common': 'ng.common',
+            '@angular/compiler': 'ng.compiler',
+            '@angular/forms': 'ng.forms',
+            '@angular/http': 'ng.http',
+            '@angular/platform-browser': 'ng.platformBrowser',
+            '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
+            'rxjs/Subject': 'Rx',
+            'rxjs/observable/PromiseObservable': 'Rx',
+            'rxjs/operator/toPromise': 'Rx.Observable.prototype',
+            'rxjs/Observable': 'Rx',
+            'rxjs/Rx': 'Rx',
+            'rxjs/add/operator/map': 'Rx.Operator.map',
+            'rxjs/add/operator/catch': 'Rx.Operator.catch'
+        }
+    },
     treeshake: true,
-    moduleName: 'ng2Completer',
-    sourceMap: true,
     onwarn: function (warning) {
         // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
         // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
@@ -16,23 +34,6 @@ export default {
     plugins: [
         uglify()
     ],
-    globals: {
-        '@angular/core': 'ng.core',
-        '@angular/common': 'ng.common',
-        '@angular/compiler': 'ng.compiler',
-        '@angular/forms': 'ng.forms',
-        '@angular/http': 'ng.http',
-        '@angular/platform-browser': 'ng.platformBrowser',
-        '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
-        'rxjs/Subject': 'Rx',
-        'rxjs/observable/PromiseObservable': 'Rx',
-        'rxjs/operator/toPromise': 'Rx.Observable.prototype',
-        'rxjs/Observable': 'Rx',
-        'rxjs/Rx': 'Rx',
-        'rxjs/add/operator/map': 'Rx.Operator.map',
-        'rxjs/add/operator/catch': 'Rx.Operator.catch'
-
-    },
     external: [
         '@angular/core',
         '@angular/common',
